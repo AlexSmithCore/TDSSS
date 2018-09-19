12789ba8e54980bf46092d8dff83a78f884be1b1
AlexSmithCore <43322970+AlexSmithCore@users.noreply.github.com>
2018-09-19 20:20:11 +0300

    Player

--- TopDownShooter/Assets/Scripts/PlayerControl.cs
+++ TopDownShooter/Assets/Scripts/PlayerControl.cs
@@ -7,13 +7,17 @@ public class PlayerControl : MonoBehaviour {
 	private Vector3 _inputs = Vector3.zero;
 	Rigidbody rb;
 	Animator animator;
+	Camera mainCamera;
+
 	public float playerSpeed;
+
 
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
 		animator = GetComponent<Animator>();
+		mainCamera = FindObjectOfType<Camera>();
 	}
 
 	void Update()
@@ -24,8 +28,21 @@ public class PlayerControl : MonoBehaviour {
 		
 		//animator.Play("Base", 0);
 		animator.SetFloat("Move", _inputs.magnitude);
-		animator.SetFloat("BackFor", _inputs.z);
-		animator.SetFloat("LeftRight", _inputs.x);
+		//animator.SetFloat("BackFor", _inputs.z);
+		//animator.SetFloat("LeftRight", _inputs.x);
+
+		animator.SetFloat("ForBack", + _inputs.z * Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.PI / 180)
+		+ _inputs.x * Mathf.Sin(transform.rotation.eulerAngles.y * Mathf.PI / 180));
+		animator.SetFloat("LeftRight", _inputs.x * Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.PI / 180)
+		-  _inputs.z * Mathf.Sin(transform.rotation.eulerAngles.y * Mathf.PI / 180));
+
+		Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+		float rayLenght;
+		if(groundPlane.Raycast(cameraRay, out rayLenght)){
+			Vector3 pointToLook = cameraRay.GetPoint(rayLenght);
+			transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+		}
 	}
 	
 	void FixedUpdate()
