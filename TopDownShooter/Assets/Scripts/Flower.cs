fa7a6a3d131ad6859dc64cde3df9c8a9f3717d3f
Yobjic <43323308+Yobjic@users.noreply.github.com>
2018-09-26 01:30:51 +0300

    Ugly Flower

---
+++ TopDownShooter/Assets/Scripts/Flower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour {
+
+	private Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player"){
+			animator.Play("Open", 0);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player"){
+			animator.Play("Close", 0);
+		}
+	}
+
+}
