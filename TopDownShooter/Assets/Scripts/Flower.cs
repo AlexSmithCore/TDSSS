1be437788f3135d669a31b4a6c1e664e2fdc97b6
AlexSmithCore <43322970+AlexSmithCore@users.noreply.github.com>
2018-09-26 02:14:33 +0300

    Flower

--- TopDownShooter/Assets/Scripts/Flower.cs
+++ TopDownShooter/Assets/Scripts/Flower.cs
@@ -6,14 +6,32 @@ public class Flower : MonoBehaviour {
 
 	private Animator animator;
 
+	public Light l;
+
+	private float pointToLight;
+
+	public MeshRenderer r;
+
+	public GameObject ps_lights;
+
 	void Start () {
 		animator = GetComponent<Animator>();
+		pointToLight = 0;
+		ps_lights.SetActive(false);
+	}
+
+	void Update(){
+		float trans = Mathf.Lerp(l.intensity, pointToLight, Time.deltaTime * 0.5f);
+		l.intensity = trans;
+		r.materials[0].SetColor("_EmissionColor", new Color(0.5105881f, trans, 0f, 1f)); 
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player"){
 			animator.Play("Open", 0);
+			pointToLight = 1.4f;
+			ps_lights.SetActive(true);
 		}
 	}
 
@@ -21,6 +39,8 @@ public class Flower : MonoBehaviour {
 	{
 		if (other.tag == "Player"){
 			animator.Play("Close", 0);
+			pointToLight = 0;
+			ps_lights.SetActive(false);
 		}
 	}
 
