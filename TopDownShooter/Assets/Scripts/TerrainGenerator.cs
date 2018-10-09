9cdf1e6492f4c476e7840b519ee35b1af1ee2823
Yobjic <43323308+Yobjic@users.noreply.github.com>
2018-10-09 20:32:15 +0300

    Landscape Shader

--- TopDownShooter/Assets/Scripts/TerrainGenerator.cs
+++ TopDownShooter/Assets/Scripts/TerrainGenerator.cs
@@ -2,48 +2,104 @@
 
 public class TerrainGenerator : MonoBehaviour {
 
-	public float depth = 20;
-	public int width = 256;
-	public int height = 256;
+	public float R; // Коэффициент скалистости
+	public int GRAIN=8; // Коэффициент зернистости
+	public bool FLAT = false; // Делать ли равнины
+	public Material material; 
 
-	public float scale = 20f;
+	private int width=2048;
+	private int height=2048;
+	private float WH;
+	private Color32[] cols;
+	private Texture2D texture;
 
-	public Texture2D[] textures;
+	
+	void Start () 
+	{
+		int resolution = width;
+		WH = (float)width+height;
 
-	public Vector2 offset;
+		// Задаём карту высот
+		Terrain terrain = FindObjectOfType<Terrain> ();
+		float[,] heights = new float[resolution,resolution]; 
 
-	public GameObject[] trees;
+		// Создаём карту высот
+		texture = new Texture2D(width, height);
+		cols = new Color32[width*height];
+		drawPlasma(width, height);
+		texture.SetPixels32(cols);
+		texture.Apply();
 
-	private float lastOffset;
-	public float treesOffset;
+		// Используем шейдер
+		material.SetTexture ("_HeightTex", texture);
 
-	void Start(){
-		Terrain terrain = GetComponent<Terrain>();
-		offset = new Vector2(Random.Range(0f, 9999f),Random.Range(0f, 9999f));
-		terrain.terrainData = GenerateTerrain(terrain.terrainData);
-	}
-
-	TerrainData GenerateTerrain (TerrainData terrainData){
-		terrainData.size = new Vector3(width, depth, height);
-		terrainData.SetHeights(0,0, GenerateHeights());
-		return terrainData;
-	}
-
-	float[,] GenerateHeights(){
-		float[,] heights = new float[width,height];
-
-		for( int x = 0; x < width; x++){
-			for(int y = 0; y < height; y++){
-				heights[x,y] = CalculateHeight(x,y);
+		// Задаём высоту вершинам по карте высот
+		for (int i=0; i<resolution; i++) {
+			for (int k=0;k<resolution; k++){
+				heights[i,k] = texture.GetPixel(i,k).grayscale*R;
 			}
 		}
 
-		return heights;
+		// Применяем изменения
+		terrain.terrainData.size = new Vector3(width, width, height);
+		terrain.terrainData.heightmapResolution = resolution;
+		terrain.terrainData.SetHeights(0, 0, heights);
 	}
 
-	float CalculateHeight (int x, int y){
-		float xCoord = (float)x / width * scale + offset.x;
-		float yCoord = (float)y / height * scale + offset.y;
-		return Mathf.PerlinNoise(xCoord, yCoord);
+        // Считаем рандомный коэффициент смещения для высоты
+	float displace(float num)
+	{
+		float max = num / WH * GRAIN;
+		return Random.Range(-0.5f, 0.5f)* max;
+	}
+	
+        // Вызов функции отрисовки с параметрами
+	void drawPlasma(float w, float h) 
+	{
+		float c1, c2, c3, c4;
+		
+		c1 = Random.value;
+		c2 = Random.value;
+		c3 = Random.value;
+		c4 = Random.value;
+		
+		divide(0.0f, 0.0f, w , h , c1, c2, c3, c4);
+	}
+	
+        // Сама рекурсивная функция отрисовки
+	void divide(float x, float y, float w, float h, float c1, float c2, float c3, float c4)
+	{
+		
+		float newWidth = w * 0.5f;
+		float newHeight = h * 0.5f;
+		
+		if (w < 1.0f && h < 1.0f)
+		{
+			float c = (c1 + c2 + c3 + c4) * 0.25f;
+			cols[(int)x+(int)y*width] = new Color(c, c, c);
+		}
+		else
+		{
+			float middle =(c1 + c2 + c3 + c4) * 0.25f + displace(newWidth + newHeight);
+			float edge1 = (c1 + c2) * 0.5f;
+			float edge2 = (c2 + c3) * 0.5f;
+			float edge3 = (c3 + c4) * 0.5f;
+			float edge4 = (c4 + c1) * 0.5f;
+
+			if(!FLAT){
+				if (middle <= 0)
+				{
+					middle = 0;
+				}
+			else if (middle > 1.0f)
+				{
+					middle = 1.0f;
+				}
+			}
+			divide(x, y, newWidth, newHeight, c1, edge1, middle, edge4);
+			divide(x + newWidth, y, newWidth, newHeight, edge1, c2, edge2, middle);
+			divide(x + newWidth, y + newHeight, newWidth, newHeight, middle, edge2, c3, edge3);
+			divide(x, y + newHeight, newWidth, newHeight, edge4, middle, edge3, c4);
+		}
 	}
 }
---
+++ TopDownShooter/Assets/Shaders/LandscapeShader.shader
@@ -0,0 +1,80 @@
+﻿Shader "Custom/LandscapeShader" {
+	Properties {
+		_Color ("Color", Color) = (0,0,0,0)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_BaseNorm ("Base Normal", 2D) = "bump" {}
+		_FirstTex ("First (RGB)", 2D) = "white" {}
+		_FirstNorm ("First Normal", 2D) = "bump" {}
+		_SecondTex ("Second (RGB)", 2D) = "white" {}
+		_SecondNorm ("Second Normal", 2D) = "bump" {}
+		_ThirdTex ("Third (RGB)", 2D) = "white" {}
+		_ThirdNorm ("Third Normal", 2D) = "bump" {}
+		_FourthTex ("Fourth (RGB)", 2D) = "white" {}
+		_FourthNorm ("Fourth Normal", 2D) = "bump" {}
+		_MixTex ("Mix", 2D) = "white" {}
+		//_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		//_Metallic ("Metallic", Range(0,1)) = 0.0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Lambert
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		//#pragma target 3.0
+
+		sampler2D _MainTex;
+		sampler2D _BaseNorm;
+		sampler2D _FirstTex;
+		sampler2D _FirstNorm;
+		sampler2D _SecondTex;
+		sampler2D _SecondNorm;
+		sampler2D _ThirdTex;
+		sampler2D _ThirdNorm;
+		sampler2D _FourthTex;
+		sampler2D _FourthNorm;
+		sampler2D _MixTex;
+
+		struct Input {
+			float2 uv_MainTex;
+			float2 uv_MixTex;
+		};
+
+		//half _Glossiness;
+		//half _Metallic;
+		fixed4 _Color;
+
+		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+		// #pragma instancing_options assumeuniformscaling
+		UNITY_INSTANCING_BUFFER_START(Props)
+			// put more per-instance properties here
+		UNITY_INSTANCING_BUFFER_END(Props)
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			// Albedo comes from a texture tinted by color
+			fixed4 cMain = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+			fixed4 cFirst = tex2D (_FirstTex, IN.uv_MainTex);
+			fixed4 cSecond = tex2D (_SecondTex, IN.uv_MainTex);
+			fixed4 cThird = tex2D (_ThirdTex, IN.uv_MainTex);
+			fixed4 cFourth = tex2D (_FourthTex, IN.uv_MainTex);
+
+			fixed4 nMain = tex2D (_BaseNorm, IN.uv_MainTex);
+			fixed4 nFirst = tex2D (_FirstNorm, IN.uv_MainTex);
+			fixed4 nSecond = tex2D (_SecondNorm, IN.uv_MainTex);
+			fixed4 nThird = tex2D (_ThirdNorm, IN.uv_MainTex);
+			fixed4 nFourth = tex2D (_FourthNorm, IN.uv_MainTex);
+
+			fixed4 cMix = tex2D (_MixTex, IN.uv_MixTex);
+
+			o.Albedo = (lerp(cMain, cFirst, cMix.r) + lerp(cMain, cSecond, cMix.g) + lerp(cMain, cThird, cMix.b) + lerp(cMain, cFourth, cMix.a));
+			o.Normal =  (lerp(nMain, nFirst, cMix.r) + lerp(nMain, nSecond, cMix.g) + lerp(nMain, nThird, cMix.b) + lerp(nMain, nFourth, cMix.a));
+			o.Alpha = cMain.a;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
