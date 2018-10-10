8c285d6d0d2aec3bcfb97d53a927f83909a5a036
Yobjic <43323308+Yobjic@users.noreply.github.com>
2018-10-10 20:57:06 +0300

    Landscape Generator

--- TopDownShooter/Assets/Scripts/MaterialGenerator.cs
+++ TopDownShooter/Assets/Scripts/MaterialGenerator.cs
@@ -7,55 +7,82 @@ public class MaterialGenerator : MonoBehaviour {
 	public Material material; 
 
 	public float delta = 1f;
+	public float R = 1f;
 
-	private int width=1024;
-	private int height=1024;
-	//private Color32[] cols;
+	private int width=4096;
+	private int widthMesh=256;
 	private Color[] pix;
-	//private Texture2D texture;
 	private Texture2D noiseTex;
 	
 	void Start () {
-		//texture = new Texture2D(width, height);
-		//cols = new Color32[width*height];
-		noiseTex = new Texture2D(width, height);
-        pix = new Color[noiseTex.width * noiseTex.height];
 
-		/*cols[0] = new Color(Random.Range(0,255), Random.Range(0,255), Random.Range(0,255), Random.Range(0,255));
-		for (int i = 1; i < cols.Length; i++){
-			if (i > width){
-				cols[i] = new Color((cols[i-1].r + cols[i-width].r)/2 + Random.Range(-delta, delta), (cols[i-1].g + cols[i-width].g)/2 + Random.Range(-delta, delta), (cols[i-1].b + cols[i-width].b)/2 + Random.Range(-delta, delta), (cols[i-1].a + cols[i-width].a)/2 + Random.Range(-delta, delta));
-			}
-			else
-			{
-				cols[i] = new Color(cols[i-1].r + Random.Range(-delta, delta), cols[i-1].g + Random.Range(-delta, delta), cols[i-1].b + Random.Range(-delta, delta), cols[i-1].a + Random.Range(-delta, delta));
-			}
-		}*/
+		MeshFilter mf = GetComponent<MeshFilter>();
+		Mesh mesh = new Mesh();
+		mf.mesh = mesh;
+
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+		MeshCollider col = GetComponent<MeshCollider>();
+
+		Vector3[] vertices = new Vector3[widthMesh * widthMesh];
+		int[] tri = new int[(widthMesh - 1)*(widthMesh - 1) * 6];
+		int j = 0;
+		Vector3[] normals = new Vector3[widthMesh * widthMesh];
+		Vector2[] uv = new Vector2[widthMesh * widthMesh];
+
+		noiseTex = new Texture2D(width, width);
+        pix = new Color[noiseTex.width * noiseTex.height];
 
 		CalcNoise();
 
-
-		//texture.SetPixels32(cols);
-		//texture.Apply();
-
 		material.SetTexture ("_MixTex", noiseTex);
+
+		for (int i=0; i<widthMesh; i++) {
+			for (int k=0;k<widthMesh; k++){
+				vertices[i * widthMesh + k] = new Vector3(k, noiseTex.GetPixel(k * (width - 1) / (widthMesh - 1), i * (width - 1) / (widthMesh - 1)).grayscale*R, i);
+				normals[i * widthMesh + k] = Vector3.up;
+				if (k < widthMesh - 1 && i < widthMesh - 1){
+					tri[j] = i * widthMesh + k;
+					tri[j+1] = (i + 1) * widthMesh + k;
+					tri[j+2] = (i) * widthMesh + k + 1;
+
+					tri[j+3] = (i + 1) * widthMesh + k;
+					tri[j+4] = (i + 1) * widthMesh + k + 1;
+					tri[j+5] = (i) * widthMesh + k + 1;
+					
+					j += 6;
+				}
+				uv[i * widthMesh + k] = new Vector2(vertices[i * widthMesh + k].x, vertices[i * widthMesh + k].z);
+			}
+		}
+
+		Debug.Log(j);
+
+		mesh.vertices = vertices;
+		mesh.triangles = tri;
+		mesh.normals = normals;
+		mesh.uv = uv;
+
+		mr.material = material;
+
+		col.sharedMesh = mesh;
 	}
 
 	void CalcNoise() {
         float y = 0.0F;
-		float ranR = Random.value;
-		float ranG = Random.value;
-		float ranB = Random.value;
-		float ranA = Random.value;
+		float ranR = Random.Range(0.1f, 2f);
+		float ranG = Random.Range(0.1f, 2f);
+		float ranB = Random.Range(0.1f, 2f);
+		float ranA = Random.Range(0.1f, 2f);
+		Debug.Log(ranR + " " + ranG + " " + ranB + " " + ranA);
         while (y < noiseTex.height) {
             float x = 0.0F;
             while (x < noiseTex.width) {
                 float xCoord = x / noiseTex.width * delta;
                 float yCoord = y / noiseTex.height * delta;
-                float sampleR = 0.25f * (Mathf.Sin(Mathf.PerlinNoise(xCoord, yCoord) * x * ranR) + 1) * (Mathf.Sin(Mathf.PerlinNoise(xCoord, yCoord) * y * ranG) + 1);
-				float sampleG = 0.25f * (Mathf.Sin(Mathf.PerlinNoise(xCoord, yCoord) * x * ranG) + 1) * (Mathf.Sin(Mathf.PerlinNoise(xCoord, yCoord) * y * ranB) + 1);
-				float sampleB = 0.25f * (Mathf.Sin(Mathf.PerlinNoise(xCoord, yCoord) * x * ranB) + 1) * (Mathf.Sin(Mathf.PerlinNoise(xCoord, yCoord) * y * ranA) + 1);
-				float sampleA = 0.25f * (Mathf.Sin(Mathf.PerlinNoise(xCoord, yCoord) * x * ranA) + 1) * (Mathf.Sin(Mathf.PerlinNoise(xCoord, yCoord) * y * ranR) + 1);
+                float sampleR = Func(x / delta, y / delta, ranR, ranG);
+				float sampleG = Func(x / delta, y / delta, ranG, ranB);
+				float sampleB = Func(x / delta, y / delta, ranB, ranA);
+				float sampleA = Func(x / delta, y / delta, ranA, ranR);
                 pix[(int)(y * noiseTex.width + x)] = new Color(sampleR, sampleG, sampleB, sampleA);
 
                 x++;
@@ -65,4 +92,8 @@ public class MaterialGenerator : MonoBehaviour {
         noiseTex.SetPixels(pix);
         noiseTex.Apply();
     }
+
+	float Func(float a, float b, float c, float d){
+		return Mathf.Pow(Mathf.Sin(c * a * Mathf.PerlinNoise(a, b)), 2) * Mathf.Pow(Mathf.Sin(d * b * Mathf.PerlinNoise(b, a)), 2);
+	}
 }
--- TopDownShooter/Assets/Shaders/LandscapeShader.shader
+++ TopDownShooter/Assets/Shaders/LandscapeShader.shader
@@ -21,10 +21,10 @@
 
 		CGPROGRAM
 		// Physically based Standard lighting model, and enable shadows on all light types
-		#pragma surface surf Lambert
+		#pragma surface surf StandardSpecular
 
 		// Use shader model 3.0 target, to get nicer looking lighting
-		//#pragma target 3.0
+		#pragma target 3.0
 
 		sampler2D _MainTex;
 		sampler2D _BaseNorm;
@@ -54,7 +54,7 @@
 			// put more per-instance properties here
 		UNITY_INSTANCING_BUFFER_END(Props)
 
-		void surf (Input IN, inout SurfaceOutput o) {
+		void surf (Input IN, inout SurfaceOutputStandardSpecular o) {
 			// Albedo comes from a texture tinted by color
 			fixed4 cMain = tex2D (_MainTex, IN.uv_MainTex) * _Color;
 			fixed4 cFirst = tex2D (_FirstTex, IN.uv_MainTex);
